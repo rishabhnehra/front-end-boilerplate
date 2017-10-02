fc52a504c40e81242b6b9a4af1bc3dab6c1022cd
rishabhnehra <rishabhnehrapersonal@gmail.com>
2017-10-03 03:51:14 +0530

    Added ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+/* eslint-env node */
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const browserSync = require('browser-sync').create()
+const eslint = require('gulp-eslint')
 
 gulp.task('default', ['serve'])
 
@@ -15,9 +17,17 @@ gulp.task('styles', () => {
 		.pipe(browserSync.stream())
 })
 
-gulp.task('serve', ['styles'], () => {
+gulp.task('lint', () => { 
+	gulp.src(['./js/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError())
+})
+
+gulp.task('serve', ['styles', 'lint'], () => {
 	browserSync.init({
 		server: './'
 	})
-	gulp.watch('scss/*.scss', ['styles'])
+	gulp.watch('./scss/*.scss', ['styles'])
+	gulp.watch('./js/*.js', ['lint'])
 })
